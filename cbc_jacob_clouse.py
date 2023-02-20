59194278e3cb378d63a55d7104863db726763e65
jc366512 <jclouse@albany.edu>
2023-02-20 09:59:17 -0500

    created the source files for cbc and ofb programs

---
+++ cbc_jacob_clouse.py
@@ -0,0 +1,66 @@
+# Jacob Clouse, 2023 - This Python Program was written on Windows 10 and Linux Mint using VScode, your milage may vary based on your OS and configuration.
+
+'''
+This program will impliment:
+ ______    _______   ______      
+/_____/\ /_______/\ /_____/\     
+\:::__\/ \::: _  \ \\:::__\/     
+ \:\ \  __\::(_)  \/_\:\ \  __   
+  \:\ \/_/\\::  _  \ \\:\ \/_/\  
+   \:\_\ \ \\::(_)  \ \\:\_\ \ \ 
+    \_____\/ \_______\/ \_____\/ 
+                                 
+(Cipher Block Chaining)
+'''
+
+# GOALS for Development:
+# 1st: get the CBC program working, then OFB program working
+# 2nd: 
+# 3rd: 
+# 4th: 
+# 5th: 
+# 6th: 
+
+# =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+# Importing Libraries / Modules 
+# =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+
+import datetime # used to get the datetime for "defang_datetime" function
+#import random # one time pad use
+
+# =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+# Variables
+# =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+
+
+# =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+# Functions
+# =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+
+# --- Function to print out my Logo ---
+def myLogo():
+    print("Created and Tested by: ")
+    print("   __                  _         ___ _                       ")
+    print("   \ \  __ _  ___ ___ | |__     / __\ | ___  _   _ ___  ___  ")
+    print("    \ \/ _` |/ __/ _ \| '_ \   / /  | |/ _ \| | | / __|/ _ \ ")
+    print(" /\_/ / (_| | (_| (_) | |_) | / /___| | (_) | |_| \__ \  __/ ")
+    print(" \___/ \__,_|\___\___/|_.__/  \____/|_|\___/ \__,_|___/\___| ")
+    print("Dedicated to Peter Zlomek and Harley Alderson III")
+
+
+# --- Function to Defang date time ---
+def defang_datetime():
+    current_datetime = f"_{datetime.datetime.now()}"
+
+    current_datetime = current_datetime.replace(":","_")
+    current_datetime = current_datetime.replace(".","-")
+    current_datetime = current_datetime.replace(" ","_")
+    
+    return current_datetime
+
+
+# =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+# MAIN PROGRAM
+# =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+
+
