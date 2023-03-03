f2701e0321cf3659adfd520c54dbf4245a84ea48
jacobpclouse <clouse.jp@gmail.com>
2023-03-03 11:39:40 -0500

    reorganized directories and created the OFB directory under Compiles

---
+++ Compiles/OFB/OFB_Test_Snip.java
@@ -0,0 +1,17 @@
+import java.util.Random;
+import java.io.*;
+import java.util.*;
+import java.security.SecureRandom;
+
+public class OFB_Test_Snip {
+
+    public static void main(String[] args){
+        System.out.println("OFB Test");
+       
+
+
+
+    }
+
+
+}
