b2f19c4033b61175af1999a06968f244a70a18c7
jacobpclouse <clouse.jp@gmail.com>
2023-03-03 17:06:27 -0500

    finishing up with ofb stuff

--- Compiles/OFB/OFB_Test_Snip.java
+++ Compiles/OFB/OFB_Test_Snip.java
@@ -25,38 +25,52 @@ public class OFB_Test_Snip {
         }
         // Convert Nonce to integers for our incriment operation + string 
         long Long_Noonce = Long.parseLong(String_Noonce);
-
-
-        System.out.println(IV_Noonce);
-        System.out.println(String_Noonce);
-        System.out.println(Long_Noonce);
+        // System.out.println(IV_Noonce);
+        // System.out.println(String_Noonce);
+        // System.out.println(Long_Noonce);
         
 
-/*
+
+
+        // SAMPLE
+        byte[] bloc = Arrays.copyOf(IV_Noonce,16);
+        System.out.println("BLOCK: ");
+        System.out.println(bloc);
+
 
         // Simulated encrypt block
-        for (int i = 0; i < 100; i++) {
+        for (int i = 99; i < 100; i++) {
             // Getting current index to sync our counter, converting from string to bytes and cutting it at 16
-            Integer_Noonce += i;
-            String_Noonce = String.format("%016d", Integer_Noonce);
+            Long_Noonce += i;
+            System.out.println(Long_Noonce);
+
+            String_Noonce = String.format("%016d", Long_Noonce);
             System.out.println(String_Noonce);
 
             IV_Noonce = String_Noonce.getBytes();
             IV_Noonce = Arrays.copyOf(IV_Noonce, 16);
             System.out.println(IV_Noonce);
 
+            // ENCRYPTION HERE
+            // block encrypt (IV_NONCE, key)
 
+
+           
+            // Math.min(a, b);
             // XOR function: Reverse
-            // for (int a = IV_Noonce.length; a > 0; a--) {
-            //     IV_Noonce[a] = (byte) (IV_Noonce[a] ^ Counter_for_Noonce[a]);
-            // }
-
-
+            for (int a = 0; a < bloc.length; a++) {
+                IV_Noonce[a] = (byte) (IV_Noonce[a] ^ bloc[a]);
+                System.out.println(a);
+            }
+/*
+*/
+            // SET NEW IV TO output from the encryption
+            System.out.println(' ');
             
 
 
         }
-*/
+
     }
 
 
